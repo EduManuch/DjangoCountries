ce019e72147687143c641c2ea12692e36c05038d
Eduard Manucharyan <madnus123@yandex.ru>
2023-05-08 16:32:33 +0300

    created models Country and Language. Migrated

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Languages(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)

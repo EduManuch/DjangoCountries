a9dda5790f39b38fb403b44b1b107d8fa09197ba
Eduard Manucharyan <madnus123@yandex.ru>
2023-05-09 13:06:59 +0300

    changed country-list, language- list and country-page from json to db

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,36 +1,33 @@
 from django.shortcuts import render
-import json
 from django.http import HttpResponseNotFound
-# from django.http import HttpResponse
-
-with open("countries.json", 'r') as f:
-    countries = json.load(f)
+from MainApp.models import Country, Language
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def home(request):
-    # return HttpResponse("<h2>Hello!!!</h2>")
     context = {'name': "HELLO WORLD !"}
     return render(request, 'index.html', context)
 
 
 def countries_list(request):
+    countries = Country.objects.all()
     context = {'countries': countries}
     return render(request, 'countries-list.html', context)
 
 
 def languages_list(request):
-    languages = set()
-    for item in countries:
-        for language in item['languages']:
-            languages.add(language)
+    languages = Language.objects.all()
     context = {'languages': languages}
     return render(request, 'languages-list.html', context)
 
 
 def country_page(request, country):
-    for item in countries:
-        if item['country'] == country:
-            context = {'country': country,
-                       'languages': item['languages']}
-            return render(request, 'country-page.html', context)
-    return HttpResponseNotFound(f"Страна {country} не найдена")
+    #                    'languages': item['languages']}
+
+    try:
+        item = Country.objects.get(name=country)
+        context = {'country': item.name}
+        return render(request, 'country-page.html', context)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f"Страна {country} не найдена")
+

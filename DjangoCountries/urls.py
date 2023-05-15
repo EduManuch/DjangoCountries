7de5d74cb71d93cb7ea2888470d87e6fa58f39ae
Eduard Manucharyan <madnus123@yandex.ru>
2023-05-15 22:37:07 +0300

    named url's

--- DjangoCountries/urls.py
+++ DjangoCountries/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from MainApp import views
 
 urlpatterns = [
-    path('', views.home),
-    path('countries-list/', views.countries_list),
-    path('languages-list/', views.languages_list),
-    path('country/<str:country>', views.country_page),
+    path('', views.home, name='home'),
+    path('countries-list/', views.countries_list, name='countries-list'),
+    path('languages-list/', views.languages_list, name='languages-list'),
+    path('country/<str:country>', views.country_page, name='country-detail'),
 ]
